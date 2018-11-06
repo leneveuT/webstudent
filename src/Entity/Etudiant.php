f39943a3f954dcbb9c03b13d91ef415b9237c5a7
leneveuT <leneveu.thibaut@gmail.com>
2018-11-06 12:03:01 +0100

    Initial commit

---
+++ src/Entity/Etudiant.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EtudiantRepository")
+ */
+class Etudiant
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $dateNaiss;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $ville;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private $numRue;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $rue;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $coPos;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $surnom;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $photo;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getDateNaiss(): ?\DateTimeInterface
+    {
+        return $this->dateNaiss;
+    }
+
+    public function setDateNaiss(\DateTimeInterface $dateNaiss): self
+    {
+        $this->dateNaiss = $dateNaiss;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getNumRue(): ?string
+    {
+        return $this->numRue;
+    }
+
+    public function setNumRue(string $numRue): self
+    {
+        $this->numRue = $numRue;
+
+        return $this;
+    }
+
+    public function getRue(): ?string
+    {
+        return $this->rue;
+    }
+
+    public function setRue(string $rue): self
+    {
+        $this->rue = $rue;
+
+        return $this;
+    }
+
+    public function getCoPos(): ?string
+    {
+        return $this->coPos;
+    }
+
+    public function setCoPos(string $coPos): self
+    {
+        $this->coPos = $coPos;
+
+        return $this;
+    }
+
+    public function getSurnom(): ?string
+    {
+        return $this->surnom;
+    }
+
+    public function setSurnom(string $surnom): self
+    {
+        $this->surnom = $surnom;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+}
---
+++ src/Migrations/Version20181106094447.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181106094447 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE etudiant ADD num_rue VARCHAR(20) NOT NULL, ADD rue VARCHAR(50) NOT NULL, ADD co_pos VARCHAR(50) NOT NULL, ADD surnom VARCHAR(50) NOT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE etudiant DROP num_rue, DROP rue, DROP co_pos, DROP surnom');
+    }
+}
