cc1cdf18214df7f55208826a778989f30287d7a5
leneveuT <leneveu.thibaut@gmail.com>
2018-11-20 09:12:31 +0100

    Ajout des validations

--- src/Entity/Etudiant.php
+++ src/Entity/Etudiant.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\EtudiantRepository")
@@ -19,9 +20,17 @@ class Etudiant
     private $id;
 
     /**
+     * @Assert\NotBlank
+     */
+
+    /**
      * @ORM\Column(type="string", length=50)
      */
     private $nom;
+
+    /**
+     * @Assert\NotBlank
+     */
 
     /**
      * @ORM\Column(type="string", length=50)
